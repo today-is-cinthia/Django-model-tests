e026b126640829647d3c5214dd40f7d130e9e8e6
cinthia <cinthiaduarte013@gmail.com>
2024-03-18 23:12:01 -0600

    django admin register models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    user = models.CharField(max_length= 10)
+    passw = models.CharField(max_length=20,blank= False)
+    email = models.CharField(max_length= 20)
+    def __str__(self):
+        return self.user
--- app/views.py
+++ app/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect 
+from django.contrib.auth import login, authenticate
+from .models import User
 
-# Create your views here.
 
-def index(request):
-    return HttpResponse("Hola")
+def index(request): 
+    if request.method == "POST":
+        user = request.POST["username"]
+        email = request.POST["email"]
+        passw = request.POST["password"]    
+        
+        User.objects.create(user=user, passw=passw, email=email)
+        return redirect("/home")
+    return render(request, "index.html")
+
+def home(request):
+    
+    return render(request, "home.html")
